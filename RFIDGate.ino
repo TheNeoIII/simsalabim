1c4e19fb442f9c7e947f702f939e96557c285e0d
Leonid Verhovskij <info@leonv.de>
2014-04-12 21:06:03 +0200

    first release

--- RFIDGate.ino
+++ RFIDGate.ino
@@ -12,6 +12,7 @@ Delete Mode: Search for UID in EEPROM, if present, delete
 #include <MFRC522.h>
 
 
+
 #define SS_PIN 10
 #define RST_PIN 9
 
@@ -23,15 +24,19 @@ int keyStart 	= 100; 	//storage start position for keys
 int opMode	= 0;	//0=normal,1=add,2=del
 int pinAdd	= 8;
 int pinDel	= 9;
-int pinTaster   = 2;
+int pinTaster   = 7;
 int pinRelais   = 3;
 int pinRouter   = 4;
 boolean stateAdd;
 boolean stateDel;
 boolean stateRouter;
+boolean stateTaster;
 int uid[10];
 int startPos;
 int doorSeconds = 3;
+
+boolean isOpen = false;
+unsigned long openUntil;
 
 void setup(){
 	//Debug output
@@ -153,8 +158,10 @@ void deleteUID(){
 }
 void openDoor(){
   digitalWrite(pinRelais, HIGH);
-  Timer1.initialize(1000000 * doorSeconds);
-  Timer1.attachInterrupt(doorTimer);
+  //Timer1.initialize(1000000 * doorSeconds);
+  //Timer1.attachInterrupt(doorTimer);
+  openUntil = millis()+1500;
+  isOpen = true;
 }
 void checkUID(){
   Serial.println("Check");
@@ -164,6 +171,14 @@ void checkUID(){
 }
 
 void loop(){
+	if(isOpen && millis() > openUntil){
+		digitalWrite(pinRelais, LOW);
+		isOpen = false;
+                //Setup rfid module
+                SPI.end();
+	        SPI.begin();               // Init SPI bus
+                mfrc522.PCD_Init();        // Init MFRC522 card
+	}
         uid[0] = 0x00;
         uid[1] = 0x00;
         uid[2] = 0x00;
@@ -177,6 +192,12 @@ void loop(){
         stateAdd = digitalRead(pinAdd);
         stateDel = digitalRead(pinDel);
         stateRouter = digitalRead(pinRouter);
+        stateTaster = digitalRead(pinTaster);
+        
+        if(!stateTaster){
+          openDoor();
+        }
+        
 	// Look for new cards
         if ( ! mfrc522.PICC_IsNewCardPresent()) {
             return;
@@ -201,14 +222,13 @@ void loop(){
           saveUID();
         }else if(stateDel){
           deleteUID();
-        }
-        else{
+        }else{
           checkUID();
         }
         
         Serial.print("Num Keys:");
         Serial.println(numEntries());
         
-        delay(250);
+        delay(50);
 }
 
